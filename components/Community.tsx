0f8dafdac0c1a26c9db0f3320addf57384638688
evanryuu <evanryuu@outlook.com>
2024-01-19 23:25:20 +0800

    feat: infinite scroll

--- components/Community.tsx
+++ components/Community.tsx
@@ -1,178 +1,71 @@
 "use client";
 
-import { useInViewport } from "ahooks";
-import { useRef } from "react";
+import { PostData } from "@/types";
+import { useInViewport, useInfiniteScroll, useRequest, useVirtualList } from "ahooks";
+import { Data } from "ahooks/lib/useInfiniteScroll/types";
+import { useMemo, useRef, useState } from "react";
 
+interface Result {
+  list: string[];
+  nextId: string | undefined;
+}
+function getHeight(str: string) {
+  const rows = Math.ceil(str.length / 40);
+  const height = 32 * rows;
+  return height;
+}
 const CoinCommunity = () => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const scroll = useInViewport(ref);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const limit = useRef(0);
+  const [value, onChange] = useState<number>(0);
+
+  const { data, loading, loadMore, loadingMore, noMore } = useInfiniteScroll((d) => fetchData(), {
+    target: containerRef,
+    isNoMore: (d) => d?.nextId === limit.current + 1,
+  });
+
+  const [list, scrollTo] = useVirtualList(data?.list || [], {
+    containerTarget: containerRef,
+    wrapperTarget: wrapperRef,
+    itemHeight: (_v, i) => getHeight(i.body),
+  });
+  function fetchData(): Promise<Data> {
+    return fetch("https://dummyjson.com/posts?" + `skip=${data?.nextId - 1 || 0}`)
+      .then((res) => res.json())
+      .then((data: PostData) => {
+        limit.current = data.total;
+        return { list: data.posts, nextId: data.skip + data.limit + 1 };
+      });
+  }
+
   return (
-    <div id="community" className="coin-community" ref={ref}>
-      <div className="flex items-center flex-col">
-        <div className="">Community</div>
-        <div className="">Community</div>
-        <div className="">Community</div>
-        <p>{JSON.stringify(scroll)}</p>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore reiciendis officia nihil itaque doloribus
-          quae distinctio non qui, suscipit nulla voluptas unde id repellendus rem quam, saepe soluta animi. Ducimus?
-        </div>
+    <div id="community" className="coin-community" ref={containerRef}>
+      <div ref={wrapperRef}>
+        {list
+          ? list.map(({ data: ele }, i) => (
+              <div
+                data-index={ele.id}
+                className="flex-col"
+                style={{
+                  height: getHeight(ele.body),
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  border: "1px solid #e8e8e8",
+                  marginBottom: 8,
+                }}
+                key={ele.id}
+              >
+                <div>id: {ele.id}</div>
+                <div>{ele.body.slice(120)}</div>
+                {/* Row: {ele.id}
+                <div>{ele.title}</div>
+                <div>{ele.body}</div> */}
+                {/* {JSON.stringify(ele)} */}
+              </div>
+            ))
+          : null}
       </div>
     </div>
   );
